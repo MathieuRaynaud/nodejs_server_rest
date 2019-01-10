83b252bbf0736b427d5117e2f7508d2c100fe3ce
tehem <tehema.teiti@gmail.com>
2019-01-10 01:52:51 +0100

    fix access control allow header

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require("express");
 var app = express();
 const request = require('request');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 // define GET
 app.get("/query", (req, res, next) => {
     // prepare request
